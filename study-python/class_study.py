615ed50a60f80e8be5022a343ae0b2335dcb1ec1
menmenmeng <75026772+menmenmeng@users.noreply.github.com>
2022-03-04 21:46:28 +0900

    etc

--- study-python/class_study.py
+++ study-python/class_study.py
@@ -72,3 +72,51 @@ print(b.lastname)
 Family.lastname = '박'
 print(a.lastname)
 print(b.lastname)
+
+
+# 클래스 상속 - 상속받아서 새로 만들어진 클래스는 이전 클래스의 변수도 가지고 있다.
+class MyClass:
+	def __init__(self):
+		self.name = 'Myeonghyeon'
+
+class MyClass2(MyClass):
+    def newmethod(self, x, y):
+        print(x, y)
+        return x+y
+
+newclass = MyClass2()
+print('--클래스 상속이 변수도 상속')
+print(newclass.name)
+
+
+# 수학을 위한 메서드
+class NewString:
+    def __init__(self, string):
+        self.string = string
+    
+    def __add__(self, other_string_class):
+        new_string = self.string + other_string_class.string
+        return new_string
+    
+    def __sub__(self, other_string_class):
+        other_string = other_string_class.string
+
+        if len(self.string) > len(other_string):
+            return self.string[len(other_string):]
+
+        elif len(self.string) == len(other_string):
+            return ''
+
+        else :
+            return '-'+other_string[len(self.string):]
+
+
+one = NewString('Son Myeonghyeon')
+two = NewString('Crawdads')
+thr = NewString('data_analysis is fun')
+
+print('--수학을 위한 특수 메서드')
+print(one + two)
+print(one - two)
+print(one - thr)
+print(one - one)
